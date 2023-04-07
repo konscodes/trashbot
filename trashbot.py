2f20ec399015844c4452a98ef94cbe0c24954a89
Kons <katashi.rain@gmail.com>
2023-04-07 19:16:44 +0900

    Fixed linting issues

--- trashbot.py
+++ trashbot.py
@@ -1,19 +1,21 @@
+'''This bot will send a reoccurring reminders on garbage collection schedule'''
+from os.path import dirname, abspath
+from os import environ
+import json
+import logging
+import logging.config
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from os.path import dirname, abspath
-from os import environ
-import logging
-import logging.config
 from flask import Flask, request, abort
-import json
+
 
 # Build paths inside the project
 BASE_DIR = dirname(abspath(__file__))
 LOG_PATH = BASE_DIR + '/logs/logger.log'
 
 # Read JSON and configure logging using dictionary
-with open(BASE_DIR + '/logging_conf.json', 'r') as f:
+with open(BASE_DIR + '/logging_conf.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
     data['handlers']['file']['filename'] = LOG_PATH
     logging.config.dictConfig(data)
@@ -31,30 +33,35 @@ handler = WebhookHandler(str(environ.get('LINE_CHANNEL_SECRET')))
 
 @app.route('/')
 def test():
+    '''This is a test function to check if the bot is running'''
+    custom_logger.debug('This is a test')
     #custom_logger.debug(f'Request headers: \n{request.headers}')
     return 'OK'
 
 @app.route('/callback', methods=['POST'])
 def callback():
-    custom_logger.debug(f'Request headers: \n{request.headers}')
+    '''This is the main function that handles the webhook'''
+    custom_logger.debug('Request headers: \n%s', request.headers)
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    custom_logger.debug('Request body: \n' + body)
+    custom_logger.debug('Request body: \n%s', body)
 
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        custom_logger.exception('Invalid signature. Please check your channel access token/channel secret.')
+        custom_logger.exception('Invalid signature. \
+            Please check your channel access token/channel secret.')
         abort(400)
     return 'OK'
 
 # Main bot function
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    '''This function will handle all messages sent to the bot'''
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
