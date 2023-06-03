af2ab732d97bb35e3b6a76208087cd602f68ea8b
Kons M <kons@SierraPro.local>
2023-06-03 23:47:21 +0900

    Added time to the rotation functions and json data

--- roster.py
+++ roster.py
@@ -11,6 +11,8 @@ This snippet shows how to read and write json data.
 '''
 import json
 import logging
+from datetime import datetime
+
 custom_logger = logging.getLogger('custom')
 
 def check_duty(file_path: str, specific_duty) -> tuple:
@@ -38,6 +40,22 @@ def check_schedule(duties: list, duty: str) -> str:
     return [x['schedule'] for x in duties if x['name'] == duty]
 
 
+def time_difference(start_time: object, end_time: object) -> tuple:
+    '''Return the time difference between two dates in weeks and months'''
+    difference = end_time - start_time
+    weeks = difference.days // 7
+    months = difference.days // 30
+    return weeks, months
+
+
+def rotate_index(index: int, number: int, list_length: int) -> int:
+    '''Returns the new index as a result of rotating within the list range'''
+    new_index = (index + number) % list_length
+    if new_index >= list_length:
+        new_index -= list_length
+    return new_index
+
+
 def rotate_duty(file_path: str, duty: str) -> None:
     '''Rotate the duty for the next team and write the updated data to file.
     file_path: path to the json file
@@ -45,15 +63,21 @@ def rotate_duty(file_path: str, duty: str) -> None:
     '''
     with open(file_path) as file_object:
         data = json.load(file_object)
-
+    
+    schedule = data['duties'][duty]
     team_on_duty, team_on_duty_id, members_on_duty = check_duty(file_path, duty)
+    
     if team_on_duty:
+        current = datetime.now()
+        last = datetime.strptime(data['updated'], '%Y-%m-%d %H:%M:%S.%f')
+        weeks, months = time_difference(last, current)
+        if schedule == 'weekly':
+            new_id = rotate_index(team_on_duty_id, weeks, len(data['teams']))
+        elif schedule == 'monthly':
+            new_id = rotate_index(team_on_duty_id, months, len(data['teams']))
         data['teams'][team_on_duty_id]['duty'].remove(duty)
-        if team_on_duty_id < len(data['teams']) - 1:
-            data['teams'][team_on_duty_id + 1]['duty'].append(duty)
-        else:
-            data['teams'][0]['duty'].append(duty)
-
+        data['teams'][new_id]['duty'].append(duty)
+        data['updated'] = str(current)
         with open(file_path, 'w') as file_object:
             json.dump(data, file_object, indent=2, ensure_ascii=False)
     return team_on_duty, team_on_duty_id, members_on_duty, duty
